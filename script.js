1c563483d391cc36827d2c1191e8e7dbb76f79d8
Daniele63 <battle1000@hotmail.com>
2025-03-30 16:26:51 +0200

    Changed instructions text

--- script.js
+++ script.js
@@ -334,7 +334,10 @@ elements.INSTRUCTIONS_BUTTON.addEventListener("click", () => {
       "You can play with the picture settings by pressing the buttons under the screen, hover your mouse on each button to see a description of what they do." +
       "\n" +
       "\n" +
-      "This CRT TV was created by @Daniele63 at daniele63.com, please do NOT copy paste this on your website, make something unique instead!"
+      "This CRT TV was created by @Daniele63 at daniele63.com",
+    "\n" +
+      "\n" +
+      "Visit the GitHub page if you want to save it locally on your machine: https://github.com/the-daniele/crt-tv/tree/main"
   );
 });
 
